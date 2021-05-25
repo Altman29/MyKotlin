3ad5340f47970d3f0af5f9930ca845134ea32320
ccsy <chen092900@163.com>
2021-05-25 23:21:04 +0800

    feat: new chapter

--- lib_kt_summary/src/main/java/com/altman29/lib_kt_summary/MyClass.kt
+++ lib_kt_summary/src/main/java/com/altman29/lib_kt_summary/MyClass.kt
@@ -4,7 +4,14 @@ package com.altman29.lib_kt_summary
 data class Person(val name: String, val age: Int? = null)
 
 fun main() {
-    val persons = listOf<Person>(Person("Alice"), Person("Bob", 29))
+    val persons = listOf<Person>(Person("Alice"), Person("Bob", 29), Person("Til"))
     val oldest = persons.maxByOrNull { it.age ?: 0 }
     println("The oldest is: $oldest")
 }
+
+/**
+数据类 data class 自动生成toString方法
+可空类型 Int? 实参的默认值
+fun main 顶层函数
+{it.age?:0} lambda表达式；Elvis运算符
+ */
